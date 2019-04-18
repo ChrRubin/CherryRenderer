f940595ca67c13385fce43f5e00cb1f56d80aa39
ChrRubin <chrrubinmc@gmail.com>
2019-04-18 19:27:25 +0800

    Set http.agent property on program start

--- src/main/java/com/chrrubin/cherryrenderer/CherryRenderer.java
+++ src/main/java/com/chrrubin/cherryrenderer/CherryRenderer.java
@@ -54,6 +54,7 @@ public class CherryRenderer extends Application {
     }
 
     public static void main(String[] args) {
+        System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
         launch(args);
     }
 
